6771c6e1c05644d4b5694663dd5cd22c29b57ca2
Aud4city-Pr0 <zachary.duriancik@gmail.com>
2026-02-14 23:12:13 -0500

    added todays auto changes in emergency commit

--- src/autons.cpp
+++ src/autons.cpp
@@ -438,7 +438,7 @@ void SkillsAuto() {
   chassis.pid_wait();
   // scoring
   IndexerMech.set_pistion_status(PistionIndexerLib::INDEXER_OPEN);
-  pros::delay(2000);
+  pros::delay(3000);
   IndexerMech.set_pistion_status(PistionIndexerLib::INDEXER_CLOSED);
   IntakeMech.set_intake_status(false);
   // going to match loader
@@ -454,12 +454,12 @@ void SkillsAuto() {
   chassis.pid_drive_set(10_in, 10);
   chassis.pid_wait();
   // going to score
-  chassis.pid_drive_set(-36_in, DRIVE_SPEED);
+  chassis.pid_drive_set(-38_in, DRIVE_SPEED);
   chassis.pid_wait();
   set_match_loader_status(false);
   // scoring
   IndexerMech.set_pistion_status(PistionIndexerLib::INDEXER_OPEN);
-  pros::delay(1000);
+  pros::delay(3000);
   IndexerMech.set_pistion_status(PistionIndexerLib::INDEXER_CLOSED);
   // stoping scoring
   IntakeMech.set_intake_status(false);
@@ -469,7 +469,60 @@ void SkillsAuto() {
   chassis.pid_wait();
   chassis.pid_drive_set(95_in, DRIVE_SPEED);
   chassis.pid_wait();
-
-
+  chassis.pid_turn_set(90_deg, TURN_SPEED);
+  chassis.pid_wait();
+  // going to match loader
+  chassis.pid_drive_set(15_in, 50);
+  chassis.pid_wait();
+  // delaying for matchloader
+  set_match_loader_status(true);
+  pros::delay(700);
+  IntakeMech.set_intake_direction(IntakeController::INTAKE_FORWARD);
+  IntakeMech.set_intake_status(true, 12000);
+  chassis.pid_drive_set(20_in, 50);
+  chassis.pid_wait();
+  chassis.pid_drive_set(10_in, 10);
+  chassis.pid_wait();
+  set_match_loader_status(false);
+  chassis.pid_drive_set(-15_in, DRIVE_SPEED);
+  // going to other side
+  chassis.pid_drive_set(12_in, DRIVE_SPEED);
+  chassis.pid_wait();
+  chassis.pid_turn_set(190_deg, TURN_SPEED);
+  chassis.pid_wait();
+  chassis.pid_drive_set(87_in, DRIVE_SPEED);
+  chassis.pid_wait();
+  chassis.pid_turn_set(0_deg, TURN_SPEED);
+  chassis.pid_wait();
+  chassis.pid_drive_set(15_in, DRIVE_SPEED);
+  chassis.pid_wait();
+  chassis.pid_turn_set(90_deg, DRIVE_SPEED);
+  chassis.pid_wait();
+  // scoring
+  chassis.pid_drive_set(-38_in, DRIVE_SPEED);
+  chassis.pid_wait();
+  IndexerMech.set_pistion_status(PistionIndexerLib::INDEXER_OPEN);
+  pros::delay(3000);
+  IndexerMech.set_pistion_status(PistionIndexerLib::INDEXER_CLOSED);
+  IntakeMech.set_intake_status(false);
+  chassis.pid_drive_set(15_in, 50);
+  chassis.pid_wait();
+  // delaying for matchloader
+  set_match_loader_status(true);
+  pros::delay(700);
+  IntakeMech.set_intake_direction(IntakeController::INTAKE_FORWARD);
+  IntakeMech.set_intake_status(true, 12000);
+  chassis.pid_drive_set(20_in, 50);
+  chassis.pid_wait();
+  chassis.pid_drive_set(10_in, 10);
+  chassis.pid_wait();
+  set_match_loader_status(false);
+  chassis.pid_drive_set(-38_in, DRIVE_SPEED);
+  chassis.pid_wait();
+  // scoring
+  IndexerMech.set_pistion_status(PistionIndexerLib::INDEXER_OPEN);
+  pros::delay(3000);
+  IndexerMech.set_pistion_status(PistionIndexerLib::INDEXER_CLOSED);
+  IntakeMech.set_intake_status(false);
 
 }
